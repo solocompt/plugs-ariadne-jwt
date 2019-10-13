c43b987981bd95f7c0bf190484ddc71b7028af8c
Ricardo Lobo <ricardolobo@audienciazero.org>
2019-10-13 12:42:23 +0100

    support for django 1

--- plugs_ariadne_jwt/middleware.py
+++ plugs_ariadne_jwt/middleware.py
@@ -1,10 +1,15 @@
 from . import utils
+from . import exceptions
+
+def get_token(info):
+    auth_header = info.context.META.get('HTTP_AUTHORIZATION')
+    return auth_header.split(' ')[-1]
 
 def plugs_ariadne_jwt_middleware(resolver, obj, info, **args):
     try:
-        token = info.context.headers['Authorization']
+        token = get_token(info)
         user = utils.get_user_by_token(token.split(' ')[-1])
         info.context.user = user
-    except KeyError:
+    except exceptions.JSONWebTokenError:
         pass
     return resolver(obj, info, **args)
