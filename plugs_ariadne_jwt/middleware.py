37e9f87f274ac66fc48789819258937caf26947e
Ricardo Lobo <ricardolobo@audienciazero.org>
2019-11-16 19:12:48 +0000

    resolves issue with anon requests

--- plugs_ariadne_jwt/middleware.py
+++ plugs_ariadne_jwt/middleware.py
@@ -3,13 +3,17 @@ from . import exceptions
 
 def get_token(info):
     auth_header = info.context.META.get('HTTP_AUTHORIZATION')
-    return auth_header.split(' ')[-1]
+    try:
+        return auth_header.split(' ')[-1]
+    except AttributeError:
+        return None
 
 def plugs_ariadne_jwt_middleware(resolver, obj, info, **args):
     try:
         token = get_token(info)
-        user = utils.get_user_by_token(token.split(' ')[-1])
-        info.context.user = user
+        if token:
+            user = utils.get_user_by_token(token.split(' ')[-1])
+            info.context.user = user
     except exceptions.JSONWebTokenError:
         pass
     return resolver(obj, info, **args)
